b926635b141df3c73c7a5b085c912bcaef70a0bb
bkageler <87718940+bkageler@users.noreply.github.com>
2026-04-10 15:57:13 -0500

    Animated Claude Enemy

--- Project/src/main/java/com/example/examplemod/entity/ClaudeEnemy/ClaudeEnemy.java
+++ Project/src/main/java/com/example/examplemod/entity/ClaudeEnemy/ClaudeEnemy.java
@@ -9,6 +9,11 @@ import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializer;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
@@ -24,18 +29,49 @@ import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 
 public class ClaudeEnemy extends Monster {
+
+    public static final EntityDataAccessor<Boolean> isAttackingDataAccessor = SynchedEntityData.defineId(
+            ClaudeEnemy.class,
+            EntityDataSerializers.BOOLEAN);
+    public static final EntityDataAccessor<Integer> attackTimer = SynchedEntityData.defineId(ClaudeEnemy.class,
+            EntityDataSerializers.INT);
+    public static final EntityDataAccessor<Boolean> isIdleDataAccessor = SynchedEntityData.defineId(
+            ClaudeEnemy.class,
+            EntityDataSerializers.BOOLEAN);
+    public static final EntityDataAccessor<Integer> idleTimer = SynchedEntityData.defineId(ClaudeEnemy.class,
+            EntityDataSerializers.INT);
+
     public ClaudeEnemy(EntityType<? extends Monster> type, Level level) {
         super(type, level);
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, false));
+        final ClaudeEnemy entity = this;
+        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, false) {
+            @Override
+            public boolean canPerformAttack(LivingEntity target) {
+                boolean canAttack = super.canPerformAttack(target);
+                if (canAttack) {
+                    entity.getEntityData().set(isAttackingDataAccessor, true);
+                    entity.getEntityData().set(attackTimer, 20);
+                }
+                return canAttack;
+            }
+        });
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal(this, Player.class, true));
         this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(4, new NearestAttackableTargetGoal(this, LivingEntity.class, true));
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this) {
+
+            @Override
+            public void start() {
+                super.start();
+                entity.getEntityData().set(isIdleDataAccessor, true);
+                entity.getEntityData().set(idleTimer, 60);
+            }
+        });
         super.registerGoals();
     }
 
@@ -45,5 +81,37 @@ public class ClaudeEnemy extends Monster {
                 .add(Attributes.ATTACK_DAMAGE, 5.0D)
                 .add(Attributes.ATTACK_SPEED, 1.0D)
                 .add(Attributes.MOVEMENT_SPEED, 0.3D);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (!this.level().isClientSide()) {
+            // System.out.println("attackTimer: " + attackTimer);
+            // System.out.println("idleTimer: " + idleTimer);
+            setUpAnimations();
+        }
+    }
+
+    private void setUpAnimations() {
+        if (this.getEntityData().get(attackTimer) > 0) {
+            this.getEntityData().set(attackTimer, this.getEntityData().get(attackTimer) - 1);
+        } else {
+            this.getEntityData().set(isAttackingDataAccessor, false);
+        }
+        if (this.getEntityData().get(idleTimer) > 0) {
+            this.getEntityData().set(idleTimer, this.getEntityData().get(idleTimer) - 1);
+        } else {
+            this.getEntityData().set(isIdleDataAccessor, false);
+        }
+    }
+
+    @Override
+    protected void defineSynchedData(SynchedEntityData.Builder builder) {
+        super.defineSynchedData(builder);
+        builder.define(isAttackingDataAccessor, false);
+        builder.define(isIdleDataAccessor, false);
+        builder.define(attackTimer, 0);
+        builder.define(idleTimer, 0);
     }
 }
--- Project/src/main/java/com/example/examplemod/entity/ClaudeEnemy/ClaudeEnemyRenderer.java
+++ Project/src/main/java/com/example/examplemod/entity/ClaudeEnemy/ClaudeEnemyRenderer.java
@@ -7,17 +7,23 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import com.mojang.blaze3d.vertex.PoseStack;
+
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.state.CameraRenderState;
 
 public class ClaudeEnemyRenderer extends LivingEntityRenderer<ClaudeEnemy, ClaudeEnemyRenderState, ClaudeEnemyModel> {
+    // private ClaudeEnemyRenderState renderState = new ClaudeEnemyRenderState();
+
     public ClaudeEnemyRenderer(EntityRendererProvider.Context context) {
         super(context, new ClaudeEnemyModel(context.bakeLayer(cs124uiuc.CLAUDE_ENEMY_LAYER)), 0.5f);
         this.addLayer(new ClaudeEnemyRenderLayer(this, context.getModelSet()));
@@ -25,11 +31,24 @@ public class ClaudeEnemyRenderer extends LivingEntityRenderer<ClaudeEnemy, Claud
 
     @Override
     public ClaudeEnemyRenderState createRenderState() {
-        return new ClaudeEnemyRenderState();
+        return new ClaudeEnemyRenderState(); // this render seems to be active for all ClaudeEntities
     }
 
     @Override
     public void extractRenderState(ClaudeEnemy entity, ClaudeEnemyRenderState state, float partialTick) {
+        // System.out.println("isAttacking: " +
+        // entity.getEntityData().get(entity.isAttackingDataAccessor));
+        // System.out.println("isIdle: " +
+        // entity.getEntityData().get(entity.isIdleDataAccessor));
+        if (entity.getEntityData().get(entity.isAttackingDataAccessor)) {
+            state.attackAnimationState
+                    .startIfStopped(entity.tickCount - entity.getEntityData().get(entity.attackTimer));
+        } else if (entity.getEntityData().get(entity.isIdleDataAccessor)) {
+            state.idleAnimationState.startIfStopped(entity.tickCount - entity.getEntityData().get(entity.idleTimer));
+        } else {
+            state.idleAnimationState.stop();
+            state.attackAnimationState.stop();
+        }
         super.extractRenderState(entity, state, partialTick);
     }
 
