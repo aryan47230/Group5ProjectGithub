58415a11d70c097c0c9e3b1befd1957b5f1bb4cc
NO AI <57426277+AntonioAntonio-ai@users.noreply.github.com>
2026-04-25 14:48:36 -0500

    New one 12

--- Project/src/main/java/com/example/examplemod/item/ModItems.java
+++ Project/src/main/java/com/example/examplemod/item/ModItems.java
@@ -38,6 +38,9 @@ public class ModItems {
     public static final DeferredHolder<Item, SpawnEggItem> GOOSE_SPAWN_EGG;
     public static final DeferredItem<Item> GOLDEN_EGG;
     public static final DeferredItem<Item> EVIL_EGG;
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.registerSimpleItem("raw_bismuth");
+    public static final DeferredItem<Item> BISMUTH = ITEMS.registerSimpleItem("bismuth");
+    public static final DeferredItem<Item> CHISEL = ITEMS.registerSimpleItem("chisel");
 
     public ModItems() {
     }
@@ -47,8 +50,21 @@ public class ModItems {
     }
 
     static {
-        GOOSE_SPAWN_EGG = ITEMS.register("goose_spawn_egg", () -> new SpawnEggItem((new Item.Properties()).spawnEgg((EntityType)ModEntities.GOOSE.get()).setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("cs124uiuc", "goose_spawn_egg")))));
-        GOLDEN_EGG = ITEMS.registerSimpleItem("golden_egg", (props) -> props.component(DataComponents.CONSUMABLE, Consumable.builder().consumeSeconds(2.0F).animation(ItemUseAnimation.EAT).sound(SoundEvents.ARMOR_EQUIP_CHAIN).soundAfterConsume(SoundEvents.ARMOR_EQUIP_CHAIN).hasConsumeParticles(true).onConsume(new UsePortalConsumeEffect(0.5F)).build()));
+        GOOSE_SPAWN_EGG = ITEMS.register("goose_spawn_egg",
+                () -> new SpawnEggItem((new Item.Properties()).spawnEgg((EntityType) ModEntities.GOOSE.get())
+                        .setId(ResourceKey.create(Registries.ITEM,
+                                ResourceLocation.fromNamespaceAndPath("cs124uiuc", "goose_spawn_egg")))));
+        GOLDEN_EGG = ITEMS.registerSimpleItem(
+                "golden_egg",
+                props -> props.component(
+                        DataComponents.CONSUMABLE,
+                        Consumable.builder()
+                                .consumeSeconds(2f)
+                                .animation(ItemUseAnimation.EAT)
+                                .hasConsumeParticles(false)
+                                .onConsume(
+                                        new UsePortalConsumeEffect())
+                                .build()));
         EVIL_EGG = ITEMS.registerSimpleItem("evil_egg", (props) -> props.component(DataComponents.CONSUMABLE, Consumable.builder().consumeSeconds(2.0F).animation(ItemUseAnimation.EAT).sound(SoundEvents.ARMOR_EQUIP_CHAIN).soundAfterConsume(SoundEvents.ARMOR_EQUIP_CHAIN).hasConsumeParticles(true).onConsume(
                 // When finished consuming, applies the effects with a 30% chance
                 new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 600, 0), 1.0F)
